2150e422929cdca558f1de91426fa5d1db3b2614
Varun Agrawal <varagrawal@gmail.com>
2012-12-16 22:04:58 +0530

    Add info to Encode

--- Assembler/Encode.cpp
+++ Assembler/Encode.cpp
@@ -1,3 +1,12 @@
+/***************************
+	Author: Varun Agrawal
+	Prog: Encode.cpp
+
+	This program reads a text file consisting of the mnemonics 
+	and prints it out in the specified file so that it can be hardcoded
+	as an array in the Assembler program.
+***************************/
+
 #include<iostream>
 #include<fstream>
 
